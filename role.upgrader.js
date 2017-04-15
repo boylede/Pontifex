ef91f9b3ae71c53d507ee55b9c68a8ba6063e427
Daniel Boyle <boylede@gmail.com>
2017-04-15 00:02:13 -0700

    remove unnecessary code, listing changes

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,29 +1,18 @@
-
-var changeRole = function(creep, role) {
-    if (creep.memory.tenured == true) {
-        creep.say('⚡ ' + role);
-    } else {
-        //creep.memory.role = role;
-        creep.say('🔄 ' + role);
-        //creep.memory.upgrading = false;
-        //require('role.' + role).run(creep, undefined);
-    }
-}
-
+var s = require('shared');
 var roleUpgrader = {
     run: function(creep, stageController) {
         var m = creep.memory;
 
-        if(m.upgrading && creep.carry.energy == 0) {
+        if(m.upgrading && creep.carry.energy === 0) {
             m.upgrading = false;
             creep.say('🔄 refill');
-	    }
-	    if(!m.upgrading && creep.carry.energy == creep.carryCapacity) {
-	        m.upgrading = true;
-	        creep.say('⚡ upgrade');
-	    }
+        }
+        if(!m.upgrading && creep.carry.energy == creep.carryCapacity) {
+            m.upgrading = true;
+            creep.say('⚡ upgrade');
+        }
 
-	    if(m.upgrading) {
+        if(m.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
@@ -43,24 +32,26 @@ var roleUpgrader = {
                 });
             }
             
-            if (sources.length === 0 ) { 
-                return creep.memory.role = 'lost';
+            if (sources.length === 0 ) {
                 console.error('upgrader got out of room');
+                creep.memory.role = 'lost';
+                return;
             }
             var err;
             var source = creep.pos.findClosestByRange(sources);
-            if (source.structureType == STRUCTURE_CONTAINER || source.structureType == STRUCTURE_STORAGE) { 
+            if (source.structureType == STRUCTURE_CONTAINER || source.structureType == STRUCTURE_STORAGE) {
                 err = creep.withdraw(source, RESOURCE_ENERGY);
             } else {
                 err = source.transferEnergy(creep, creep.carryCapacity);
             }
             if(err == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-            } else if (err == ERR_NOT_ENOUGH_ENERGY) {
-                changeRole(creep, 'harvester');
-            } 
+            }
+            // else if (err == ERR_NOT_ENOUGH_ENERGY) {
+            //     changeRole(creep, 'harvester');
+            // } 
         }
-	}
+    }
 };
 
 module.exports = roleUpgrader;
