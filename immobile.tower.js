9c12c3347a2434ecc8bf624e18b0312e5cc4a491
Daniel Boyle <boylede@gmail.com>
2017-04-14 23:34:21 -0700

    learn from watching adjacent rooms battle -> give priority to attacking healers.

--- immobile.tower.js
+++ immobile.tower.js
@@ -1,22 +1,25 @@
 var s = require('shared');
-
- var towerLoop = function towerLoop(tower) {
+ 
+var towerLoop = function towerLoop(tower) {
      //return;
      //console.log('test');
      var targets = [];
      //var f = tower.room.find;
      //console.log(tower.room.find(FIND_STRUCTURES, {filter: () => true}).length);
      var err = OK;
-    targets = tower.room.find(FIND_HOSTILE_CREEPS);
-    if (targets.length !== 0) err = tower.attack(targets[0]);
+    targets = tower.room.find(FIND_CREEPS, {filter: (cre) => !cre.my && cre.getActiveBodyparts(HEAL) > 0});
+    if (targets.length === 0 ) {
+        targets = tower.room.find(FIND_HOSTILE_CREEPS);
+    }
     if (targets.length === 0 ) {
         targets = tower.room.find(FIND_HOSTILE_STRUCTURES);
-        if (targets.length !== 0) err = tower.attack(targets[0]);
     }
     if (targets.length === 0) {
         targets = tower.room.find(FIND_HOSTILE_CONSTRUCTION_SITES);
-        if (targets.length !== 0) err = tower.attack(targets[0]);
     }
+    if (targets.length !== 0) {
+        err = tower.attack(targets[0]);
+    } else {
     if (targets.length === 0) {
         targets = tower.room.find(FIND_MY_STRUCTURES, {filter: (str) => {str.hits < str.hitsMax && str.structureType != STRUCTURE_RAMPART} });
         if (targets.length !== 0) {
@@ -57,6 +60,7 @@ var s = require('shared');
         };
     }
     //console.log('found targets: ' + targets.length);
+    }
     
     switch(err) {
         case OK:
