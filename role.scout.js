08dd3e496eeb1ed5f4bd6d2cf169cd103061a09d
Daniel Boyle <boylede@gmail.com>
2017-04-14 23:40:34 -0700

    slightly repurpose this role to scope out potential sites for expansion.

--- role.scout.js
+++ role.scout.js
@@ -6,10 +6,30 @@ var scout = {
         var m = creep.memory;
         
         var destination = new RoomPosition(22, 32, Memory.attack);
-        var path = {visualizePathStyle: {stroke: '#ffaa00', opacity: 1.0}};
-        if (creep.room.name == Memory.attack) {
-            //creep.moveTo(creep.room.controller);
-            err = creep.signController(creep.room.controller, 'Kilroy was here.');
+        var path = {visualizePathStyle: {plainCost: 1, swampCost: 1, stroke: '#ffaa00', opacity: 1.0}};
+        
+        let room = creep.room.name;
+         if (Memory.rooms[room].ticksTravelTime === undefined) {
+                Memory.rooms[room].ticksTravelTime = 1500 - creep.ticksToLive;
+            }
+        if (room == Memory.attack) {
+            // creep.moveTo(creep.room.controller);
+            // if (Memory.rooms[room].ticksTravelTime === undefined) {
+            //     Memory.rooms[room] = {ticksTravelTime: 1500 - creep.ticksToLive};
+            // }
+            Memory.attack = 'E22S83';
+            //err = creep.signController(creep.room.controller, 'Planned Expansion');
+            err = creep.moveTo(creep.room.controller, path);
+            if(creep.pos.getRangeTo(creep.room.controller) < 2) {
+                Memory.rooms[room].ticksTravelTimeToController = 1500 - creep.ticksToLive;
+            }
+            // if(creep.getActiveBodyparts(false && CLAIM) > 0) {
+            //     if (false & Game.gcl.level > 1) {
+            //         err = creep.claimController(creep.room.controller);
+            //     } else {
+            //         err = creep.reserveController(creep.room.controller);
+            //     }
+            // }
         } else {
             err = creep.moveTo(destination, path);
         }
