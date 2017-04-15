f9be675ac9f2524b75f7e8502554e078d9aa8f54
Daniel Boyle <boylede@gmail.com>
2017-04-15 00:03:20 -0700

    make builder faster, change priorities to match strength of economy.

--- stage.stable.js
+++ stage.stable.js
@@ -1,17 +1,18 @@
 var work = [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY];
+var build = [WORK, MOVE, WORK, MOVE, WORK, MOVE, CARRY, MOVE, CARRY, MOVE,];
 var heavy = [WORK, MOVE, WORK, MOVE, WORK, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY];
 var extract = [MOVE, WORK, WORK, WORK, WORK, WORK, CARRY];
 var logistic = [CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY];
 var defend = [MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK];
-var scout = [MOVE];
+var scout = [MOVE]; // , CLAIM, MOVE, CLAIM
 var raider = [MOVE, ATTACK];
 module.exports = {
     desiredHarvesters: 0,
     desiredFatHarvesters: 2,
     desiredFatUpgraders: 1,
     desiredUpgraders: 0,
-    desiredBuilders: 0,
-    desiredSherpas: 3,
+    desiredBuilders: 1,
+    desiredSherpas: 4,
     desiredDefenders: 0,
     desiredScouts: 0,
     desiredRaiders: 0,
@@ -22,7 +23,7 @@ module.exports = {
     fatUpgrader: heavy,
     upgrader: work,
     defender: defend,
-    builder: work,
+    builder: build,
     sherpa: logistic,
     hitsWall: 250000,
     hitsRampart: 250000,
