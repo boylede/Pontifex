a4207446e0ac4e8ea2d7d496bec272da8a8e76fa
Daniel Boyle <boylede@gmail.com>
2017-04-15 00:09:19 -0700

    remove references to removed 'utils' module, linting changes

--- main.js
+++ main.js
@@ -11,8 +11,7 @@ var immobileHarvester = require('immobile.harvester');
 var immobileUpgrader = require('immobile.upgrader');
 var roleScout = require('role.scout');
 var roleRaider = require('role.raider');
-var u = require('utils');
-var s = require('shared.creep');
+var s = require('shared');
 
 var debugStyle = {align: 'left'};
 
@@ -24,21 +23,21 @@ function countRole(role) {
 
 module.exports.loop = function () {
     PathFinder.use(true);
-    u.setup();
+    s.setup();
     var stage = '';
     var spawn = s.getSpawn();
     //s.structSay(spawn, 'hi');
     if (spawn === undefined) {
         return OK;
     }
-    if ( Memory.fixedCreeps == false) {
-        u.fixCreepsInMemory();
+    if ( Memory.fixedCreeps === false) {
+        s.fixCreepsInMemory();
         Memory.fixedCreeps = true;
         console.log('fixed creeps');
     }
     for (var r in Game.rooms) {
         var room = Game.rooms[r];
-        if (room.setup != true) {
+        if (room.setup !== true) {
              //Memory.spawns = {};
             var SPAWNS = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
             for (let spawnName in SPAWNS) {
@@ -101,34 +100,35 @@ module.exports.loop = function () {
     var fatHarvesters = countRole('fatHarvester');
     var fatUpgraders = countRole('fatUpgrader');
     var scouts = countRole('scout');
-    var scouts = countRole('raider');
+    var raiders = countRole('raider');
     
     
     var stageC = stageController.stages[spawn.room.memory.stage];
     //Memory.creepCounter = {harvesters: harvesters.length, upgraders: upgraders.length, builders: builders.length};
     // console.log(stageC.name);
-    if(spawn.spawning) { 
+    var err = OK;
+    if(spawn.spawning) {
         var spawningCreep = Game.creeps[spawn.spawning.name];
-        s.structSay(spawn, 
+        s.structSay(spawn,
             '🛠️' + spawningCreep.memory.role);
     } else if(harvesters.length < stageC.desiredHarvesters) {
-       var err = spawn.createCreep(stageC.harvester, undefined, {role: 'harvester'});
+       err = spawn.createCreep(stageC.harvester, undefined, {role: 'harvester'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Harvester.');
-       };
+       }
     } else if(fatHarvesters.length < stageC.desiredFatHarvesters) {
-       var err = spawn.createCreep(stageC.fatHarvester, undefined, {role: 'fatHarvester'});
+       err = spawn.createCreep(stageC.fatHarvester, undefined, {role: 'fatHarvester'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning fatHarvester.');
-       };
+       }
     } else if (sherpas.length < stageC.desiredSherpas) {
-        var err = spawn.createCreep(stageC.sherpa, undefined, {role: 'sherpa'});
+        err = spawn.createCreep(stageC.sherpa, undefined, {role: 'sherpa'});
         if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Sherpa.');
-        };
+        }
     } else if (upgraders.length < stageC.desiredUpgraders) {
         if (false) {
             err = spawn.createCreep(stageC.upgrader, undefined, {role: 'upgrader', tenured: true, tenuredRole:'upgrader'});
@@ -139,37 +139,37 @@ module.exports.loop = function () {
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Upgrader.');
-       };
+       }
     } else if (fatUpgraders.length < stageC.desiredFatUpgraders) {
         err = spawn.createCreep(stageC.fatUpgrader, undefined, {role: 'fatUpgrader'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning fatUpgrader.');
-       };
+       }
     } else if (builders.length < stageC.desiredBuilders) {
-       var err = spawn.createCreep(stageC.builder, undefined, {role: 'builder'});
+       err = spawn.createCreep(stageC.builder, undefined, {role: 'builder'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Builder.');
-       };
+       }
     } else if (defenders.length < stageC.desiredDefenders) {
-       var err = spawn.createCreep(stageC.defender, undefined, {role: 'defender'});
+       err = spawn.createCreep(stageC.defender, undefined, {role: 'defender'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Defender.');
-       };
+       }
     } else if (scouts.length < stageC.desiredScouts) {
-       var err = spawn.createCreep(stageC.scout, undefined, {role: 'scout'});
+       err = spawn.createCreep(stageC.scout, undefined, {role: 'scout'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Scout.');
-       };
+       }
     } else if (false && raiders.length < stageC.desiredRaiders) {
-       var err = spawn.createCreep(stageC.raider, undefined, {role: 'raider'});
+       err = spawn.createCreep(stageC.raider, undefined, {role: 'raider'});
        if (err != OK) {
            s.structErr(spawn, err);
            console.log('error while spawning Raider.');
-       };
+       }
     }
 
     for(var name in Game.creeps) {
@@ -185,11 +185,11 @@ module.exports.loop = function () {
                 roleUpgrader.run(creep, stageC);
                 break;
             case 'builder':
-                roleBuilder.run(creep, stageC)
+                roleBuilder.run(creep, stageC);
                 break;
             case 'defective':
                 roleDefective.run(creep, stageC);
-                break
+                break;
             case 'sherpa':
                 roleSherpa.run(creep, stageC);
                 break;
@@ -210,4 +210,4 @@ module.exports.loop = function () {
                 break;
         }
     }
-}
+};
