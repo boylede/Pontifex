d1bc1327787b39dfa71d7953914917fb493675fc
Daniel Boyle <boylede@gmail.com>
2017-04-15 21:20:40 -0700

    change staging to be more responsive / controllable on a per-stage basis

--- controller.stage.js
+++ controller.stage.js
@@ -1,64 +1,24 @@
-var stages = ['seed', 'boom', 'mature', 'boom', 'stable'];
+var stages = ['seed', 'boom', 'mature', 'stable']; // rush
 var stageModules = {};
 stages.forEach(function(e,i,a) {
   stageModules[e] = require('stage.' + e );
     //console.log('loaded ' + e + ' stage.');
   });
 var change = function(stage) {
-    console.log('changing stage to ' + stage);
-    //stageModules[current].buildConstructionSites();
-    return stage;      
-}
+  console.log('changing stage to ' + stage);
+  return stage;
+};
+
 var stage = function(room) {
-    //console.log('checking stage of ' + room.name);
   if ( Memory.rooms[room.name] === undefined) {
-      console.log('new room ' + room.name);
+    console.log('new room ' + room.name);
     Memory.rooms[room.name] = {};
   }
-  var mroom = Memory.rooms[room.name];
-  //console.log(mroom.stage);
+  const mroom = Memory.rooms[room.name];
   var current = mroom.stage || 'seed';
-  //return current;
-//  console.log('managing stage ' + current);
-let numExtensions = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_EXTENSION}).length;
-  switch(current) {
-    case 'seed':
-//        console.log('evaluating stage');
-    if (room.controller) {
-      if (room.controller.my) {
-          if (room.energyAvailable >= 550 && numExtensions >= 5) {
-          //if (Memory.rooms[room.name].creeps && Memory.rooms[room.name].creeps.length > 2) {
-              current = change('boom');
-          }
-      }
-    }
-    break;
-    case 'boom':
-        if (room.energyAvailable < 50) {
-            current = change('seed');
-        } else if (room.energyAvailable >= 900 && numExtensions >= 12) {
-            current = change('mature');
-        }
-            break;
-    case 'rush':
-    if (room.energyAvailable < 50) {
-            current = change('seed');
-        }
-            break;
-    case 'mature':
-        if ( room.energyAvailable < 12) {
-            current = change('seed');
-        } else if (room.energyAvailable >= 800 && numExtensions >= 18) {
-            current = change('stable');
-        }
-            break;
-    case 'stable':
-        if ( false && room.energyAvailable < 200) {
-            current = change('mature');
-        } else if (false && room.energyAvailable >= 1200 && numExtensions >= 18) {
-            current = change('stable');
-        }
-            break;
+  const stageC = stageModules[current];
+  if (stageC.levelUp) {
+    current = stageModules[current].levelUp(room);
   }
   mroom.stage = current;
   return current;
--- stage.seed.js
+++ stage.seed.js
@@ -1,4 +1,13 @@
 var body = [MOVE, CARRY, WORK, WORK];
+var levelUp = function levelUp(room) {
+    let spawns = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_SPAWN});
+    //let numExtensions = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_EXTENSION}).length;
+    if (spawns > 0 && room.controller.level > 1) {
+        return 'boom';
+    } else {
+        return 'seed';
+    }
+};
 module.exports = {
     tasks: [],
     creeps: {
@@ -25,5 +34,6 @@ module.exports = {
     },
     hitsWall: 1000,
     hitsRampart: 1000,
-    name:'seed'
+    name:'seed',
+    levelUp: levelUp
 };
