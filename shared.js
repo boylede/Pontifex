6711ac19b1a5ce133c57c4dcca2fe82deda7deee
Daniel Boyle <boylede@gmail.com>
2017-04-15 17:43:21 -0700

    fix references to shared functions inside other shared functions

--- shared.js
+++ shared.js
@@ -1,3 +1,46 @@
+const debugStyle = {
+    align: 'left'
+};
+const ERRORS = [
+    'OK',                         // 0
+    'ERR_NOT_OWNER',               // 1
+    'ERR_NO_PATH',                 // 2
+    'ERR_NAME_EXISTS',             // 3
+    'ERR_BUSY',                    // 4
+    'ERR_NOT_FOUND',               // 5
+    'ERR_NOT_ENOUGH_RESOURCES',    // 6
+    'ERR_INVALID_TARGET',          // 7
+    'ERR_FULL',                    // 8
+    'ERR_NOT_IN_RANGE',            // 9
+    'ERR_INVALID_ARGS',            //10
+    'ERR_TIRED',                   //11
+    'ERR_NO_BODYPART'              //12
+    ];
+
+const COOLERRORS = [
+    'OK',            // 0
+    'NOT MINE!',               // 1
+    'NO WAY!',                 // 2
+    'WHO?!',             // 3
+    'BUSY!',                    // 4
+    '404!',               // 5
+    'POOR! SAD!',    // 6
+    '403!',          // 7
+    'FULL!',                    // 8
+    'TOO FAR!',            // 9
+    'WHAT?!',            //10
+    'ZZZZZZ',                   //11
+    'RAMSEY!'              //12
+    ];
+
+const energyAtLocation = function energyAtLocation(location) {
+        var energy = src.room.lookForAt(LOOK_ENERGY, location);
+        if (energy !== undefined || energy !== null) {
+            return energy[0].amount;
+        } else {
+            return 0;
+        }
+    };
 module.exports = {
     currentVersion: '0.0.0.11',
     setup: function setup() {
@@ -19,51 +62,18 @@ module.exports = {
         Memory.creeps = contain;
         return OK;
     },
-    debugStyle: {align: 'left'},
-    ERRORS: ['OK',                  // 0
-     'ERR_NOT_OWNER',               // 1
-     'ERR_NO_PATH',                 // 2
-     'ERR_NAME_EXISTS',             // 3
-     'ERR_BUSY',                    // 4
-     'ERR_NOT_FOUND',               // 5
-     'ERR_NOT_ENOUGH_RESOURCES',    // 6
-     'ERR_INVALID_TARGET',          // 7
-     'ERR_FULL',                    // 8
-     'ERR_NOT_IN_RANGE',            // 9
-     'ERR_INVALID_ARGS',            //10
-     'ERR_TIRED',                   //11
-     'ERR_NO_BODYPART'              //12
-     ],
-    COOLERRORS:  ['OK',            // 0
-     'NOT MINE!',               // 1
-     'NO WAY!',                 // 2
-     'WHO?!',             // 3
-     'BUSY!',                    // 4
-     '404!',               // 5
-     'POOR! SAD!',    // 6
-     '403!',          // 7
-     'FULL!',                    // 8
-     'TOO FAR!',            // 9
-     'WHAT?!',            //10
-     'ZZZZZZ',                   //11
-     'RAMSEY!'              //12
-     ],
-     ERR_NO_SOURCE:  -7281,
-     ERR_NO_TARGET:  -7282,
-     getSpawn:  function getSpawn() {
+    debugStyle: debugStyle,
+    ERRORS: ERRORS,
+    COOLERRORS:  COOLERRORS,
+    ERR_NO_SOURCE:  -7281,
+    ERR_NO_TARGET:  -7282,
+    getSpawn:  function getSpawn() {
         var i;
         for (i in Game.spawns) {
             return Game.spawns[i];
         }
     },
-    energyAtLocation:  function energyAtLocation(location) {
-        var energy = src.room.lookForAt(LOOK_ENERGY, location);
-        if (energy !== undefined || energy !== null) {
-            return energy[0].amount;
-        } else {
-            return 0;
-        }
-    },
+    energyAtLocation:  energyAtLocation,
     isEnergyEnough:  function isEnergyEnough(constructionSite) {
         var required = constructionSite.progressTotal - constructionSite.progress;
         return required <= energyAtLocation(constructionSite);
