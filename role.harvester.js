c65f2bfcf5c793808f5c9661f1c908599ea5cb67
Daniel Boyle <boylede@gmail.com>
2017-04-15 18:27:35 -0700

    cleanup

--- role.harvester.js
+++ role.harvester.js
@@ -1,41 +1,25 @@
 var s = require('shared');
 var getSource = function(creep) {
     var sources = creep.room.find(FIND_DROPPED_ENERGY, {filter: (resource) => resource.type = RESOURCE_ENERGY});
-    //console.log('found ' + sources.length + ' dropped energy.');
     if (sources.length === 0) {
-    sources = creep.room.find(FIND_SOURCES, {
-                    filter: (src) => src.energy > 0
-            });
+        sources = creep.room.find(FIND_SOURCES, {
+            filter: (src) => src.energy > 0
+        });
     }
-    return sources[0];
+    if (sources.length > 0) {
+        return sources[0];
+    } else {
+        console.log('no source here');
+        return {};
+    }
 };
 var roleHarvester = {
     run: function(creep, stageC) {
         var err = OK;
         var m = creep.memory;
         var source = getSource(creep);
-        // if (stageC.name == 'stable') {
-        //     m.role = 'builder';
-        //     return;
-        // }
-        // var source = Game.getObjectById(m.source);
-        // if ( source == null) {
-        //     if (m.savedSource !== undefined) {
-        //         m.source = m.savedSource;
-        //         source = Game.getObjectById(m.source);
-        //     } else {
-        //         source = getSource(creep);
-        //         m.source = source.id;
-        //     }
-        // } else {
-        //     source = Game.getObjectById(m.source);
-        // }
-        //creep.say('harvesting...');
-        
-        //todo: have creep store target in memory for quicker loops.
+
         if(creep.carry.energy < creep.carryCapacity) {
-            //var sources = creep.room.find(FIND_SOURCES);
-            //creep.say(sources.length);
             if (source.resourceType === undefined) {
                 err = creep.harvest(source);
             } else {
@@ -43,24 +27,24 @@ var roleHarvester = {
             }
             switch (err) {
                 case ERR_NOT_IN_RANGE:
-                    creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-                    break;
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+                break;
                 case ERR_FULL:
-                    source = null;
-                    break;
+                source = null;
+                break;
                 case OK:
-                    break;
+                break;
                 default:
-                        s.creepErr(creep, err);
-                        break;
+                s.creepErr(creep, err);
+                break;
             }
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                    structure.energy < structure.energyCapacity;
+                }
             });
             if (targets.length === 0) {
                 targets = creep.room.find(FIND_STRUCTURES, {
@@ -80,13 +64,10 @@ var roleHarvester = {
             
             if(targets.length > 0) {
                 let target = creep.pos.findClosestByRange(targets);
-                //console.log('hh ' + target.id);
                 if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
-                //creep.memory.role='builder';
-                //console.log('nowhere to put resource');
                 creep.say('^v^');
             }
         }
