71ade98dfa2e9167f092252ca1e70ca401ed6ce6
Daniel Boyle <boylede@gmail.com>
2017-04-18 21:13:16 -0700

    let scouts claim rooms

--- role.scout.js
+++ role.scout.js
@@ -9,27 +9,27 @@ var scout = {
         var path = {visualizePathStyle: {plainCost: 1, swampCost: 1, stroke: '#ffaa00', opacity: 1.0}};
         
         let room = creep.room.name;
-         if (Memory.rooms[room].ticksTravelTime === undefined) {
-                Memory.rooms[room].ticksTravelTime = 1500 - creep.ticksToLive;
-            }
+        if (Memory.rooms[room].ticksTravelTime === undefined) {
+            Memory.rooms[room].ticksTravelTime = 1500 - creep.ticksToLive;
+        }
         if (room == Memory.attack) {
             // creep.moveTo(creep.room.controller);
             // if (Memory.rooms[room].ticksTravelTime === undefined) {
             //     Memory.rooms[room] = {ticksTravelTime: 1500 - creep.ticksToLive};
             // }
-            Memory.attack = 'E22S83';
+            // Memory.attack = 'E22S83';
             //err = creep.signController(creep.room.controller, 'Planned Expansion');
             err = creep.moveTo(creep.room.controller, path);
             if(creep.pos.getRangeTo(creep.room.controller) < 2) {
                 Memory.rooms[room].ticksTravelTimeToController = 1500 - creep.ticksToLive;
             }
-            // if(creep.getActiveBodyparts(false && CLAIM) > 0) {
-            //     if (false & Game.gcl.level > 1) {
-            //         err = creep.claimController(creep.room.controller);
-            //     } else {
-            //         err = creep.reserveController(creep.room.controller);
-            //     }
-            // }
+            if(creep.getActiveBodyparts(CLAIM) > 0) {
+                if (Game.gcl.level > 1) {
+                    err = creep.claimController(creep.room.controller);
+                } else {
+                    err = creep.reserveController(creep.room.controller);
+                }
+            }
         } else {
             err = creep.moveTo(destination, path);
         }
@@ -38,18 +38,18 @@ var scout = {
             case ERR_FULL:
             case ERR_NOT_ENOUGH_ENERGY:
         //    case ERR_NOT_ENOUGH_RESOURCES:
-            case ERR_TIRED:
-            case OK:
-                break;
-            case ERR_NOT_IN_RANGE:
-                creep.moveTo(creep.room.controller, path);
-                break;
-            default:
-                s.creepErr(creep, err);
-            break;
-        }
-        return OK;
+        case ERR_TIRED:
+        case OK:
+        break;
+        case ERR_NOT_IN_RANGE:
+        creep.moveTo(creep.room.controller, path);
+        break;
+        default:
+        s.creepErr(creep, err);
+        break;
     }
+    return OK;
+}
 };
 
 module.exports = scout;
