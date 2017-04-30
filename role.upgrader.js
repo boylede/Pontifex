e990cdb9e4a5ddff8f0886a2adeb6c725f43fd39
Daniel Boyle <boylede@gmail.com>
2017-04-30 01:04:39 -0700

    restructure upgrader to be more readable

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,15 +1,55 @@
 var s = require('shared');
+
+var getSource = function(creep) {
+    var sources = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
+            structure.store[RESOURCE_ENERGY] >= creep.carryCapacity;
+        }
+    });
+    if (sources.length === 0) {
+        sources = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                structure.energy >= creep.carryCapacity;
+            }
+        });
+    }
+    if (sources.length === 0) {
+        sources = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE);
+            }
+        });
+    }
+    if (sources.length === 0) {
+        sources = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN);
+            }
+        });
+    }
+    if (sources.length === 0) {
+        sources = creep.room.find(FIND_SOURCES);
+    }
+    
+    if (sources.length === 0 ) {
+        console.log('upgrader lost');
+        creep.memory.role = 'defective';
+        return;
+    }
+    return creep.pos.findClosestByRange(sources);
+};
+
 var roleUpgrader = {
     run: function(creep) {
         var m = creep.memory;
 
         if(m.upgrading && creep.carry.energy === 0) {
             m.upgrading = false;
-            // creep.say('refill');
         }
         if(!m.upgrading && creep.carry.energy == creep.carryCapacity) {
             m.upgrading = true;
-            // creep.say('upgrade');
         }
 
         if(m.upgrading) {
@@ -17,45 +57,8 @@ var roleUpgrader = {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         } else {
-            var sources = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
-                            structure.store[RESOURCE_ENERGY] >= creep.carryCapacity;
-                    }
-            });
-            if (sources.length === 0) {
-                sources = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy >= creep.carryCapacity;
-                    }
-                });
-            }
-            if (sources.length === 0) {
-                sources = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE);
-                    }
-                });
-            }
-            if (sources.length === 0) {
-                sources = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN);
-                    }
-                });
-            }
-            if (sources.length === 0) {
-                sources = creep.room.find(FIND_SOURCES);
-            }
-            
-            if (sources.length === 0 ) {
-                console.log('upgrader lost');
-                creep.memory.role = 'defective';
-                return;
-            }
             var err;
-            var source = creep.pos.findClosestByRange(sources);
+            var source = getSource(creep);
             if (source) {
                 if (source.structureType == STRUCTURE_CONTAINER || source.structureType == STRUCTURE_STORAGE || source.structureType == STRUCTURE_SPAWN || source.structureType == STRUCTURE_EXTENSION) {
                     err = creep.withdraw(source, RESOURCE_ENERGY);
@@ -64,14 +67,10 @@ var roleUpgrader = {
                 } else {
                     err = source.transferEnergy(creep, creep.carryCapacity);
                 }
-
                 if(err == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
             }
-            // else if (err == ERR_NOT_ENOUGH_ENERGY) {
-            //     changeRole(creep, 'harvester');
-            // } 
         }
     }
 };
