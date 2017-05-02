fd79b5c4b3ffe6c6f9a5872299e9e47777b48c83
Daniel Boyle <boylede@gmail.com>
2017-05-01 22:49:37 -0700

    let containerupgraders use links if available

--- role.containerUpgrader.js
+++ role.containerUpgrader.js
@@ -1,29 +1,22 @@
 var s = require('shared');
 
 var freeSpace = function(src) {
-    //var src = creep.room.controller;
-    var area = src.room.lookForAtArea(LOOK_STRUCTURES, src.pos.y - 1, src.pos.x - 1, src.pos.y + 1, src.pos.x + 1, true);
-    var free = _.filter(area, (str) => {
-        let st = str.structure;
-        /*
-        console.log('looking for fat asses at ' + st.id);
-        let fatUpgraders = st.room.find(FIND_MY_CREEPS, {filter: (creep) => {
-            return creep.memory.role == 'fatUpgrader' && creep.memory.container !== undefined && creep.memory.container == st.id;
-        } });
-        console.log('found ' + fatUpgraders.length);
-        */
-        let result = st.structureType == STRUCTURE_CONTAINER;// && fatUpgraders.length === 0;
-        return result;
-    });
+    var free = _.filter(
+        src.room.lookForAtArea(LOOK_STRUCTURES, src.pos.y - 2, src.pos.x - 2, src.pos.y + 2, src.pos.x + 2, true),
+        (str) => str.structure.structureType == STRUCTURE_LINK
+        );
+    if (free.length === 0) {
+        free = _.filter(
+        src.room.lookForAtArea(LOOK_STRUCTURES, src.pos.y - 2, src.pos.x - 2, src.pos.y + 2, src.pos.x + 2, true),
+        (str) => str.structure.structureType == STRUCTURE_CONTAINER
+        );
+    }
     var free_containers = [];
     for (var name in free) {
         free_containers.push(free[name].structure);
     }
-    console.log('found ' + free_containers.length + ' free containers at ' + src.id);
-    // return free[0].structure;
     return free_containers;
 };
-
 
 function onTop(creep, container) {
     return creep.pos == container.pos;
@@ -34,37 +27,17 @@ var immobileUpgrader = {
         var err = OK;
         var m = creep.memory;
         var controller = creep.room.controller;
-        var container;
+        var container = creep.pos.findClosestByRange(freeSpace(controller));
 
-        if (m.container === undefined) {
-            let index = -1;
-            let containers = freeSpace(controller);
-            console.log('found ' + containers.length + ' containers free for upgrader ' + creep.name);
-            container = creep.pos.findClosestByRange(containers);
-            if ( container !== undefined) {
-                console.log('selected container for ' + creep.name);
-                m.container = container.id;
-                m.empty = false;
-            }
-        } else {
-            container = Game.getObjectById(m.container);
-        }
-        if (!m.empty) {
-            if (creep.pos.x == container.pos.x && creep.pos.y == container.pos.y) {
-                if (creep.carry[RESOURCE_ENERGY] > 0) {
-                    err = creep.upgradeController(controller);
-                } else {
-                    err = creep.withdraw(container, RESOURCE_ENERGY);
-                }
+        if (creep.pos.getRangeTo(container) <= 1) {
+            if (creep.carry[RESOURCE_ENERGY] > 0) {
+                err = creep.upgradeController(controller);
             } else {
-                err = creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
+                err = creep.withdraw(container, RESOURCE_ENERGY);
             }
 
             switch (err) {
                 case ERR_NOT_ENOUGH_RESOURCES:
-                    m.empty = true;
-                    // if (container.store.RESOURCE_ENERGY > 0) m.empty = false;
-                    break;
                 case ERR_TIRED:
                 case ERR_BUSY:
                 case OK:
@@ -74,8 +47,7 @@ var immobileUpgrader = {
                 break;
             }
         } else {
-            //console.log(container.store[RESOURCE_ENERGY] > 0);
-            if (container.store[RESOURCE_ENERGY] > 0) m.empty = false;
+            creep.moveTo(container);
         }
         return OK;
     }
