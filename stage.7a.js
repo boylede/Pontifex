afcf1bdc80942f97c410c3699c3694d83068fe9b
Daniel Boyle <boylede@gmail.com>
2017-05-14 16:44:25 -0700

    add 7th stage to room progression

---
+++ stage.7a.js
@@ -0,0 +1,71 @@
+var work = [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY];
+var build = [WORK, MOVE, WORK, MOVE, WORK, MOVE, CARRY, MOVE, CARRY, MOVE,];
+var heavy = [WORK, MOVE, WORK, MOVE, WORK, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY];
+var extract = [MOVE, WORK, WORK, WORK, WORK, WORK, CARRY];
+var logistic = [CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE];
+var defend = [MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK];
+var scout = [MOVE]; // , CLAIM, MOVE, CLAIM
+var raider = [MOVE, ATTACK];
+
+const level = 7;
+const maxEnergy = CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][level] * EXTENSION_ENERGY_CAPACITY[level];
+
+var levelUp = function levelUp(room) {
+    // let numTowers = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_TOWER}).length;
+    //const numLinks = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_LINK}).length;
+    // const numExt = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_EXTRACTOR}).length;
+    // const numTerm = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_TERMINAL}).length;
+    //let numContainers = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_CONTAINER}).length;
+    if (room.energyAvailable >= maxEnergy) {
+        return '7b';
+    } else if (room.controller.level < level) {
+        return '6a.duchy';
+    } else {
+        return '7a';
+    }
+};
+
+module.exports = {
+    creeps: {
+        harvester: {
+            desired: 0,
+            body: work
+        },
+        containerHarvester: {
+            desired: 2,
+            body: extract
+        },
+        upgrader: {
+            desired: 0,
+            body: work
+        },
+        containerUpgrader: {
+            desired: 1,
+            body: heavy
+        },
+        defender: {
+            desired: 0,
+            body: defend
+        },
+        sherpa: {
+            desired: 4,
+            body: logistic
+        },
+        builder: {
+            desired: 1,
+            body: build
+        },
+        scout: {
+            desired: 0,
+            body: scout
+        },
+        raider: {
+            desired: 0,
+            body: raider
+        }
+    },
+    hitsWall: 1000000,
+    hitsRampart: 1000000,
+    name:'7a',
+    levelUp: levelUp
+};
---
+++ stage.7b.js
@@ -0,0 +1,71 @@
+/*
+*/
+var work = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY];
+var build = [WORK, MOVE, WORK, MOVE, WORK, MOVE, CARRY, MOVE, CARRY, MOVE,];
+var extract = [MOVE, WORK, WORK, WORK, WORK, WORK, CARRY];
+var logistic = [CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE];
+var defend = [MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK];
+var scout = [MOVE]; // , CLAIM, MOVE, CLAIM
+var raider = [MOVE, ATTACK];
+
+const level = 7;
+const maxEnergy = CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][level] * EXTENSION_ENERGY_CAPACITY[level];
+
+var levelUp = function levelUp(room) {
+    // let numTowers = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_TOWER}).length;
+    // let numLinks = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_LINK}).length;
+    //let numContainers = room.find(FIND_MY_STRUCTURES, {filter: (str) => str.structureType == STRUCTURE_CONTAINER}).length;
+    if (room.controller.level > level && room.energyAvailable >= maxEnergy) {
+        console.log('construct additional pylons'); // need to make more levels
+        return '7b';
+    } else if (room.controller.level < level) {
+        return '6a.duchy';
+    } else {
+        return '7b';
+    }
+};
+
+module.exports = {
+    creeps: {
+        harvester: {
+            desired: 0,
+            body: work
+        },
+        containerHarvester: {
+            desired: 2,
+            body: extract
+        },
+        upgrader: {
+            desired: 0,
+            body: work
+        },
+        containerUpgrader: {
+            desired: 1,
+            body: work
+        },
+        defender: {
+            desired: 0,
+            body: defend
+        },
+        sherpa: {
+            desired: 4,
+            body: logistic
+        },
+        builder: {
+            desired: 1,
+            body: build
+        },
+        scout: {
+            desired: 0,
+            body: scout
+        },
+        miner: {
+            desired: 1,
+            body: [MOVE, WORK, CARRY]
+        }
+    },
+    hitsWall: 1000000,
+    hitsRampart: 1000000,
+    name:'7b',
+    levelUp: levelUp
+};
