fb101ebe6fae30a8f3290d3fa11d3c910d9969f5
Daniel Boyle <boylede@gmail.com>
2017-05-16 19:10:26 -0700

    remove extraneous scout code

--- role.scout.js
+++ role.scout.js
@@ -1,57 +1,43 @@
 var s = require('shared');
+var moveOpts = {plainCost: 1, swampCost: 1, visualizePathStyle: {stroke: '#ffaa00', opacity: 1.0}};
 
-var scout = {
-    run: function(creep) {
-        var err = OK;
-        var m = creep.memory;
-        
-        var destination = new RoomPosition(22, 32, Memory.attack);
-        var path = {visualizePathStyle: {plainCost: 1, swampCost: 1, stroke: '#ffaa00', opacity: 1.0}};
-        
-        let room = creep.room.name;
-        if (Memory.rooms[room].ticksTravelTime === undefined) {
-            Memory.rooms[room].ticksTravelTime = 1500 - creep.ticksToLive;
-        }
-        if (room == Memory.attack) {
-            // creep.moveTo(creep.room.controller);
-            // if (Memory.rooms[room].ticksTravelTime === undefined) {
-            //     Memory.rooms[room] = {ticksTravelTime: 1500 - creep.ticksToLive};
-            // }
-            // Memory.attack = 'E22S83';
-            //err = creep.signController(creep.room.controller, 'Planned Expansion');
-            err = creep.moveTo(creep.room.controller, path);
-            if(creep.pos.getRangeTo(creep.room.controller) < 2) {
-                Memory.rooms[room].ticksTravelTimeToController = 1500 - creep.ticksToLive;
-            }
-            if(creep.getActiveBodyparts(CLAIM) > 0) {
-                if (Game.gcl.level > 1) {
-                    err = creep.claimController(creep.room.controller);
-                } else {
-                    err = creep.reserveController(creep.room.controller);
-                }
-            } else {
-                creep.memory.role = 'builder';
-            }
-        } else {
-            err = creep.moveTo(destination, path);
-        }
-
-        switch (err) {
-            case ERR_FULL:
-            case ERR_NOT_ENOUGH_ENERGY:
-        //    case ERR_NOT_ENOUGH_RESOURCES:
-        case ERR_TIRED:
-        case OK:
-        break;
+var errResponse = function errResponse(err, creep, goal) {
+    var mem = goal;
+    switch (err) {
         case ERR_NOT_IN_RANGE:
-        creep.moveTo(creep.room.controller, path);
+        creep.moveTo(goal, moveOpts);
+        break;
+        case OK:
+        case ERR_FULL:
+        case ERR_NOT_ENOUGH_ENERGY:
+        case ERR_TIRED:
         break;
         default:
-        s.creepErr(creep, err);
+        creep.say('?');
         break;
     }
-    return OK;
-}
+    return mem;
+};
+
+var scout = {
+    run: function(creep, stageC) {
+        var err = OK;
+        var m = creep.memory;
+        var destination;
+        var room = creep.room.name;
+
+        if (m.destination) {
+            destination = new RoomPosition(m.destination.x, m.destination.y, m.destination.room);
+            isThere = m.destination.room === room;
+            if (destination) {
+                err = creep.moveTo(destination, moveOpts);
+                m.destination = errResponse(err, creep, destination);
+            }
+        }
+        m.destination = errResponse(err, creep, destination);
+        return OK;
+
+    }
 };
 
 module.exports = scout;
